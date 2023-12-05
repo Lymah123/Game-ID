c74118a0bcbec73a3fea763924c0ded4d50b1584
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-12-05 22:10:30 +0300

    Added a DID

--- server/index.js
+++ server/index.js
@@ -1,18 +1,8 @@
 import { Web5 } from '@web5/api';
-//Open app process
-//Make decision whether mobile or desktop: if else statement.
-//Make a decision whether to enter DID or ENTER inputs to join
-//If you Enter inputs to join
-//Genetate DID
-// Then set password
-//If enter DID
-// Enter Password
-//Next process
-//Register this password
-//Enter display number
-//Enter passcode to be approved.
 
 
-const { web5, did: aliceDid } = await Web5.connect();
+const { web5,did: gammyDid} = await Web5.connect();
 
-console.log(aliceDid);
+
+
+console.log(gammyDid);
