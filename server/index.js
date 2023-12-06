ec0a6fbf28af80fb2f6b9090caed022a20348e0d
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-12-06 20:47:12 +0300

    created a read DID

--- server/index.js
+++ server/index.js
@@ -3,6 +3,28 @@ import { Web5 } from '@web5/api';
 
 const { web5,did: gammyDid} = await Web5.connect();
 
+const { protocols } = await web5.dwn.protocols.query({
+    message: {
+      filter: {
+        protocol: 'https://music.org/protocol',
+      },
+    },
+  });
+
+// Reads the indicated record from the user's DWNs
+let { record } = await web5.dwn.records.read({
+  message: {
+    filter: {
+      recordId: recordId
+    }
+  }
+});
+
+// assuming the record has a text payload
+const text = await record.data.text();
+
+
+
 
 
 console.log(gammyDid);
